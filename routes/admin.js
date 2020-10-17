a2e1175b3bc641660737d56e9d620f750a208107
shihar <shiharbinsideeque73@gmail.com>
2020-10-17 23:25:47 +0530

    admin panel changed and view-product  table maked

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,41 @@ router.get('/', function(req, res, next) {
 
   products=[
     {
+      name: "IPhone 10",
+      category: "Mobile",
+      description: "It's a good phone ",
+      image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.i-uYtPWRCvFe2qJjuagRzAHaHa%26pid%3DApi&f=1"
+
+    },
+    {
+      name: "IPhone 11",
+      category: "Mobile",
+      description: "It's a good phone ",
+      image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.OOFam0AzFxbdDU_VA-40CQHaHa%26pid%3DApi&f=1"
+
+    },
+    {
+      name: "Samsung m31",
+      category: "Mobile",
+      description: "It's a good phone ",
+      image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.9MyVNGRl-r_sPL_u2Pj-xAHaHa%26pid%3DApi&f=1"
+
+    },
+    {
+      name: "Oppo f10",
+      category: "Mobile",
+      description: "It's a good phone ",
+      image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.-pbDbCzGlvvRdTQFPvLx_QHaHa%26pid%3DApi&f=1"
+
+    },
+    {
+      name: "Redmi note 9",
+      category: "Mobile",
+      description: "It's a good phone ",
+      image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.QOo9pvvbSnNM9n6ppLYNkAHaHa%26pid%3DApi&f=1"
+
+    },
+    {
       name: "vivo y50",
       category: "Mobile",
       description: "It's a good phone ",
@@ -14,7 +49,8 @@ router.get('/', function(req, res, next) {
     }
   ]
 
-res.render("index",{products, title:"Admin-Panel", admin:true})
+res.render("admin/view-products",{products, title:"Admin-Panel", admin:true})
 });
 
 module.exports = router;
+ 
